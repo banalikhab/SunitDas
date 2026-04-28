480fd0c6b0100b65faeb58ee0c4021779c5b2274
Banalikha Bhattacharjya <banalikhab@gmail.com>
2026-04-28 15:27:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello mckv')
+  res.send('Hello mckv. Banalikha!')
 })
 
 app.listen(4000, ()=>{
